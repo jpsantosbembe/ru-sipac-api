059875b6c4bbc8bf4f0505dfb9cc1c6c7e2a5c71
Joao Paulo S Bembe <joaobembe@icloud.com>
2023-07-20 22:34:09 -0300

    mais ajustes na estrutura do projeto

--- src/controller/ControladorUsuario.java
+++ src/controller/ControladorUsuario.java
@@ -12,6 +12,33 @@ public class ControladorUsuario {
         Perfil perfil = new Perfil(nomeCompleto, tipoDeVinculo, situacaoDoVinculo, URLFoto);
         Carteira carteira = new Carteira(codigoCarteira, saldoCarteira, strQRCodeCarteira);
         Credenciais credenciais = new Credenciais(nomeUsuario, senhaUsuario);
-        return new Usuario(perfil, carteira, credenciais);
+        return Usuario.getInstance(perfil, carteira, credenciais);
+    }
+    public Usuario obterUsuario() {
+        return Usuario.getInstance(null, null, null);
+    }
+    public String consultarNomeUsuario (Usuario usuario) {
+        return usuario.getPerfil().getNomeCompleto ();
+    }
+    public String consultarTipoDeVinculo (Usuario usuario) {
+        return usuario.getPerfil().getTipoDeVinculo();
+    }
+    public String consultarSituacaoDoVinculo (Usuario usuario) {
+        return usuario.getPerfil().getSituacaoDoVinculo();
+    }
+    public String consultarURLFoto (Usuario usuario) {
+        return usuario.getPerfil().getURLFoto ();
+    }
+    public String consultarCodigoCarteira (Usuario usuario) {
+        return usuario.getCarteira().getCodigo();
+    }
+    public double consultarSaldoCarteira (Usuario usuario) {
+        return usuario.getCarteira ().getSaldo ();
+    }
+    public String consultarStrQRCodeCarteira (Usuario usuario) {
+        return usuario.getCarteira ().getStrQRCode ();
+    }
+    public void atualizarSaldoCarteira (Usuario usuario, int novoSaldo) {
+        usuario.getCarteira ().setSaldo (novoSaldo);
     }
 }
--- src/model/Usuario.java
+++ src/model/Usuario.java
@@ -1,13 +1,20 @@
 package model;
 
 public class Usuario {
+    private static Usuario instance;
     private Perfil perfil;
     private Carteira carteira;
     private Credenciais credenciais;
-    public Usuario(Perfil perfil, Carteira carteira, Credenciais credenciais) {
+    private Usuario(Perfil perfil, Carteira carteira, Credenciais credenciais) {
         this.perfil = perfil;
         this.carteira = carteira;
         this.credenciais = credenciais;
+    }
+    public static Usuario getInstance(Perfil perfil, Carteira carteira, Credenciais credenciais) {
+        if (instance == null) {
+            instance = new Usuario(perfil, carteira, credenciais);
+        }
+        return instance;
     }
     public Perfil getPerfil() {
         return perfil;
